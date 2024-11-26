59dc937ca1ff0f10a410f73600ee983254cbb3e3
Yura Kozyrenko <yura.mogilev@mail.ru>
2024-11-26 23:01:45 +0300

    update mongo env

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -1,17 +1,23 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-const { HTTP_PORT, DB_HOST, MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD, DB_DATABASE, DB_AUTH_SOURCE } =
-  process.env;
+const {
+  HTTP_PORT,
+  MONGO_DB_HOST,
+  MONGO_INITDB_ROOT_USERNAME,
+  MONGO_INITDB_ROOT_PASSWORD,
+  MONGO_DB_DATABASE,
+  MONGO_DB_AUTH_SOURCE,
+} = process.env;
 export default (): any =>
   ({
     API_PREFIX: '/api',
     API_VERSION: '/v1',
     HTTP_PORT: Number(HTTP_PORT) || 8000,
     MONGO_DB_SETTINGS: {
-      host: DB_HOST,
+      host: MONGO_DB_HOST,
       username: MONGO_INITDB_ROOT_USERNAME,
       password: MONGO_INITDB_ROOT_PASSWORD,
-      database: DB_DATABASE,
-      authSource: DB_AUTH_SOURCE,
+      database: MONGO_DB_DATABASE,
+      authSource: MONGO_DB_AUTH_SOURCE,
     },
   }) as const;
